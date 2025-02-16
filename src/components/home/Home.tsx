592d29843cd61740959160ad217a58163aa93c29
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-16 12:13:45 +0000

    fonts added - LexendDeca-Regular, SpaceMono-Bold

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+/**
+ * Renders the home page 
+ * 
+ * @returns {JSX.Element} - The Home component
+ */
+const Home = ():React.JSX.Element =>{
+    return(
+       <main>Home</main> 
+    )
+}
+export default Home;
