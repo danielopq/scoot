3e7bc5739e562ecad4a4b67426aa7f0f6e9e2c43
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-20 19:16:16 +0000

    Routes defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { NavBar, Footer } from "./components/layout";
-import { Home } from "./components/pages";
+import { Home, About, Location, Careers } from "./components/pages";
 
 function App() {
   return (
@@ -8,6 +8,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Location" element={<Location />} />
+        <Route path="/Careers" element={<Careers />} />
       </Routes>
       <Footer />
     </>
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,1 +1,4 @@
 export {default as Home} from "./home/Home";
+export {default as About} from "./about/About";
+export {default as Location} from "./location/Location";
+export {default as Careers} from "./careers/Careers";
