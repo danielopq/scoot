5e7b450e1b923febaedf5676f29b1cbd52925ddc
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-20 19:04:06 +0000

    Location.tsx - created

---
+++ src/components/pages/location/Location.tsx
@@ -0,0 +1,13 @@
+/**
+ * Location component that renders the Location section of the website.
+ * 
+ * @returns {React.JSX.Element} The rendered Location component.
+ */
+const Location:React.FC = ():React.JSX.Element=>{
+    return(
+        <main id="location">
+            Location
+        </main>
+    )
+}
+export default Location;
