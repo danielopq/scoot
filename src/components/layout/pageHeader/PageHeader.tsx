9613f0056aa5563b020263e1bda4d1c5dd65977b
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-20 20:54:46 +0000

    PageHeaders.tsx - ready for all of the devices version

--- src/components/layout/pageHeader/PageHeader.tsx
+++ src/components/layout/pageHeader/PageHeader.tsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from 'react';
 import './pageHeader.css';
 
-interface PageHeaderProps{
-    title:string;
+interface PageHeaderProps {
+    title: string;
+    mobileImg: string;
+    tabletImg: string;
+    desktopImg: string;
 }
 
-const PageHeader:React.FC<PageHeaderProps> = ({title}):React.JSX.Element=>{
-    return(
-        <header className='pageHeader'>
-            <h1>{title}</h1>
+/**
+ * Component that renders a header at the top of certain sections of the website.
+ * 
+ * @param {PageHeaderProps} props - The properties for the PageHeader component.
+ * @param {string} props.title - The title displayed in the header.
+ * @param {string} props.mobileImg - The background image for mobile screens.
+ * @param {string} props.tabletImg - The background image for tablet screens.
+ * @param {string} props.desktopImg - The background image for desktop screens.
+ * @returns {React.JSX.Element} The rendered PageHeader component.
+ */
+const PageHeader: React.FC<PageHeaderProps> = ({ title, mobileImg, tabletImg, desktopImg }): React.JSX.Element => {
+
+    const [backgroundImage, setBackgroundImage] = useState<string>('');
+
+    useEffect(() => {
+        updateBackgroundImage();
+        window.addEventListener('resize', updateBackgroundImage);
+        return () => {
+            window.removeEventListener('resize', updateBackgroundImage);
+        };
+    }, [desktopImg, tabletImg, mobileImg]);
+
+    /**
+     * Updates the background image based on the current window size.
+     * Determines the screen size category (desktop, tablet, or mobile)
+     * and sets the appropriate image URL.
+     */
+    const updateBackgroundImage = () => {
+        const isDesktop = window.innerWidth >= 1024;
+        const isTablet = window.innerWidth >= 768 && window.innerWidth < 1024;
+        setBackgroundImage(isDesktop ? desktopImg : isTablet ? tabletImg : mobileImg);
+    };
+
+    return (
+        <header className='pageHeader' style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <div>
+                <h1>{title}</h1>
+            </div>
         </header>
     )
 }
--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,5 +1,7 @@
 import { PageHeader } from "../../layout";
-
+import mobileHeaderPic from '../../../assets/img/headers/about-hero-mobile.jpg';
+import tabletHeaderPic from '../../../assets/img/headers/about-hero-tablet.jpg';
+import desktopHeaderPic from '../../../assets/img/headers/about-hero-desktop.jpg';
 /**
  * About component that renders the About section of the website.
  * 
@@ -8,7 +10,7 @@ import { PageHeader } from "../../layout";
 const About:React.FC = ():React.JSX.Element=>{
     return(
         <main>
-            <PageHeader title="About"/>
+            <PageHeader title="About" mobileImg={mobileHeaderPic} tabletImg={tabletHeaderPic} desktopImg={desktopHeaderPic}/>
         </main>
     )
 }
