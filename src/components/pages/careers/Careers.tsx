6033238c8426317d8f093125e3821fd384a2aa80
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-20 19:05:17 +0000

    Careers.tsx - created

---
+++ src/components/pages/careers/Careers.tsx
@@ -0,0 +1,13 @@
+/**
+ * Careers component that renders the Careers section of the website.
+ * 
+ * @returns {React.JSX.Element} The rendered Careers component.
+ */
+const Careers:React.FC = ():React.JSX.Element=>{
+    return(
+        <main id="careers">
+            Careers
+        </main>
+    )
+}
+export default Careers;
