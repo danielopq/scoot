d4fe7fd26b9efdc309153a8784e599430d327f0d
Daniel Martinez Duque <danielopq@gmail.com>
2025-02-20 01:20:22 +0000

    feature.tsx - improved

--- src/components/pages/home/components/homeFeatures/HomeFeatures.tsx
+++ src/components/pages/home/components/homeFeatures/HomeFeatures.tsx
@@ -1,11 +1,41 @@
 import './homeFeatures.css';
 
-import { Feature } from '../../../../ui';
+import telemetryPic from './img/telemetry.jpg';
+import nearYou from './img/near-you.jpg';
+import payments from './img/payments.jpg';
 
-const HomeFeatures = ()=>{
-    return(
+import { Feature,DefaultButton } from '../../../../ui';
+
+const HomeFeatures = () => {
+    return (
         <section id="homeFeatures">
-            <Feature />
+            <div>
+                <Feature
+                    pic={telemetryPic}
+                    arrowType='leftDownBottom'
+                    header='Easy to use riding telemetry'
+                    text='The Scoot app is available with riding telemetry. This means it can show you your average speed, how long you&apos;ve been using the scooter, your traveling distance, and many more things all in an easy to use app.'
+                />
+                <DefaultButton text='Learn More'/>
+            </div>
+            <div>
+                <Feature
+                    pic={nearYou}
+                    arrowType='rightDownTop'
+                    header='Coming to a city near you'
+                    text='Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year.'
+                />
+                <DefaultButton text='Learn More'/>
+            </div>
+            <div>
+                <Feature
+                    pic={payments}
+                    arrowType='leftDownTop'
+                    header='Zero hassle payments'
+                    text='Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month.'
+                />
+                <DefaultButton text='Learn More'/>
+            </div>
         </section>
     )
 }
--- src/components/ui/feature/Feature.tsx
+++ src/components/ui/feature/Feature.tsx
@@ -1,12 +1,22 @@
 import './feature.css';
 
-interface FeatureProps{
-
+interface FeatureProps {
+    pic:string;
+    arrowType: 'leftDownBottom' | 'leftDownTop' | 'rightDownTop';
+    header: string;
+    text:string;
 }
 
-const Feature:React.FC = ():React.JSX.Element=>{
-    return(
-        <div className='feature'></div>
+const Feature: React.FC<FeatureProps> = ({pic,arrowType,header,text}): React.JSX.Element => {
+    return (
+        <div className='feature'>
+            <div className='featureTop'>
+                <div className='featureMainPic' style={{backgroundImage:`url(${pic})`}}></div>
+                <div className={`featureArrow ${arrowType}`}></div>
+            </div>
+            <h2>{header}</h2>
+            <p className='defaultText'>{text}</p>
+        </div>
     )
 }
 export default Feature;
