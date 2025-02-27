f2b52d0ea84d2d7d76121ca69304ff9e2c835c81
Daniel Martínez Duque <danielopq@gmail.com>
2025-02-27 12:30:46 +0000

    JobOffers.tsx - Finished

--- src/components/pages/careers/components/jobOffers/JobOffers.tsx
+++ src/components/pages/careers/components/jobOffers/JobOffers.tsx
@@ -1,12 +1,37 @@
 import './jobOffers.css';
 import Offer from './components/Offer';
 
+/**
+ * Displays the job offers section in the Careers area of the website.
+ * 
+ * @returns {React.JSX.Element} The JobOffers component.
+ */
 const JobOffers: React.FC = (): React.JSX.Element => {
     return (
         <section id="jobOffers">
             <Offer
                 position='General Manager'
                 location='Jakarta, Indonesia'
+            />
+            <Offer
+                position='UI/UX Designer'
+                location='Yokohama, Japan'
+            />
+            <Offer
+                position='Blog Content Copywriter'
+                location='New York, United States'
+            />
+            <Offer
+                position='Graphic Designer'
+                location='New York, United States'
+            />
+            <Offer
+                position='Fleet Supervisor'
+                location='Jakarta, Indonesia'
+            />
+            <Offer
+                position='UX Analyst'
+                location='London, United Kingdom'
             />
         </section>
     )
--- src/components/pages/careers/components/jobOffers/components/Offer.tsx
+++ src/components/pages/careers/components/jobOffers/components/Offer.tsx
@@ -1,15 +1,27 @@
 import './offer.css';
+import { DefaultButton } from '../../../../../ui';
 
-interface OfferProps{
-    position:string;
-    location:string;
+interface OfferProps {
+    position: string;
+    location: string;
 }
 
-const Offer: React.FC<OfferProps> = ({position,location}): React.JSX.Element => {
+/**
+ * Job offer component for the careers section of the website.
+ * 
+ * @param {OfferProps} props - Component properties.
+ * @param {string} props.position - Title of the job position.
+ * @param {string} props.location - Location where the job is offered.
+ * @returns {React.JSX.Element} The Offer component.
+ */
+const Offer: React.FC<OfferProps> = ({ position, location }: OfferProps): React.JSX.Element => {
     return (
         <div className='offer'>
-            <p className='offerPosition'>{position}</p>
-            <p className='defaultText'>{location}</p>
+            <div>
+                <p className='offerPosition'>{position}</p>
+                <p className='defaultText'>{location}</p>
+            </div>
+            <DefaultButton text='Apply'/>
         </div>
     )
 }
